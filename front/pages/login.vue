23ca142d3f79b3ab05efae7ea5ed07f750091af1
小明 <1838674006@qq.com>
2022-10-11 22:25:55 +0800

    dubbo-admin

--- front/pages/login.vue
+++ front/pages/login.vue
@@ -75,7 +75,7 @@
               //第四步 调用接口 根据token获取用户信息，为了首页面显示
               loginApi.getLoginUserInfo()
                 .then(response => {
-                  this.loginInfo = response.data.data.userInfo
+                  this.loginInfo = JSON.stringify(response.data.data.userInfo)
                   //获取返回用户信息，放到cookie里面
                   cookie.set('guli_ucenter',this.loginInfo,{domain: 'localhost'})
 
